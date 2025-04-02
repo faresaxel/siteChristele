e339c3ea739ef010a819fa409ed9b770a3e91003
Fares MOKRANI <fares.mokrani@external.engie.com>
2025-04-02 11:24:48 +0200

    siteWebChristele

---
+++ script.js
@@ -0,0 +1,19 @@
+// Sélectionne toutes les bulles
+const items = document.querySelectorAll('.menu-item');
+
+// Ajoute un écouteur d'événement de survol à chaque bulle
+items.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        // Lorsque la souris entre dans une bulle, réinitialise la hauteur de toutes les bulles
+        items.forEach(innerItem => {
+            innerItem.style.height = '100px'; // Retour à la taille initiale
+        });
+        // Agrandit la hauteur de la bulle survolée
+        item.style.height = '150px';
+    });
+    
+    item.addEventListener('mouseleave', () => {
+        // Lorsque la souris quitte la bulle, réinitialise la hauteur de toutes les bulles
+        item.style.height = '100px'; // Retour à la taille initiale
+    });
+});

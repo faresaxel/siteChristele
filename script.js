cd03bf06558905ee2201e781168b7aec4d27d58f
Fares MOKRANI <fares.mokrani@external.engie.com>
2025-04-03 14:38:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,19 +18,19 @@ items.forEach(item => {
     });
 });
 
-// Sélection des services
-const services = document.querySelectorAll('.service');
-
-// Ajoute un écouteur d'événement pour chaque service
-services.forEach(service => {
+// Ajoute un écouteur d'événement de survol à chaque bulle
+service.forEach(service => {
     service.addEventListener('click', () => {
-        // Supprime la classe active de tous les services
-        services.forEach(innerService => {
-            innerService.classList.remove('active');
+        // Lorsque la souris entre dans une bulle, réinitialise la hauteur de toutes les bulles
+        service.forEach(innerItem => {
+            innerItem.style.height = '100px'; // Retour à la taille initiale
         });
-        
-        // Ajoute la classe active à l'élément cliqué
-        service.classList.add('active');
+        // Agrandit la hauteur de la bulle survolée
+        service.style.height = '150px';
+    });
+    
+    item.addEventListener('mouseleave', () => {
+        // Lorsque la souris quitte la bulle, réinitialise la hauteur de toutes les bulles
+        service.style.height = '100px'; // Retour à la taille initiale
     });
 });
-
